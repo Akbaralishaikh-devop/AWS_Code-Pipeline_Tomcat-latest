c2a12fa1611614f1c76d0b59d7ef9a7fe285f587
Vinit Pania <117646262+VinitPania@users.noreply.github.com>
2023-08-18 10:48:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-aws s3 cp s3://demo-java-app-pipeline/AWS_Code-Pipeline_Tomcat_Build/LoginWebApp-1.war /tmp
+aws s3 cp s3://demo-java-app-pipeline/AWS_Code-Pipeline_Tomcat_Build/AWS_Code-Pipeline_Tomcat/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /home/ec2-user/apache-tomcat-9.0.76/webapps
 sudo /home/ec2-user/apache-tomcat-9.0.76/bin/startup.sh
 chmod -R 777 /home/ec2-user/apache-tomcat-9.0.79/webapps/LoginWebApp-1.war
